fcaedca451ce470b9f373072ebebcd89f55fa4c1
NazarHook <145909670+NazarHook@users.noreply.github.com>
2024-04-21 20:19:51 +0300

    created

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig(({ command }) => {
   return {
+    assetsInclude: ['**/*.mp4'],
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
